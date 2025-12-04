70d65f5b5ef81db30783455e82166905c8372cdf
hodinit <horatiulazar2003@gmail.com>
2025-12-04 18:00:26 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Lazar_Horatiu_Lab8_Master.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lazar_Horatiu_Lab8_Master.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lazar_Horatiu_Lab8_Master.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
