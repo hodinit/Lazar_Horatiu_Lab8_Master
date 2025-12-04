84cc1287e42ccefdb3581002c6987c9ffcee5dd0
hodinit <horatiulazar2003@gmail.com>
2025-12-04 19:40:55 +0200

    finished lab

---
+++ Lazar_Horatiu_Lab8_Master/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using MovieRecommenderGrpcService;
+
+namespace Lazar_Horatiu_Lab8_Master.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public MovieController()
+        {
+            //se va modifica portul corespunzator
+            channel = GrpcChannel.ForAddress("https://localhost:7241");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Recommend()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Recommend(float userId, float movieId)
+        {
+            var client = new
+            MovieRecommender.MovieRecommenderClient(channel);
+            var request = new RecommendationRequest
+            {
+                UserId = userId,
+                MovieId = movieId
+            };
+            var reply = client.Recommend(request);
+            ViewBag.UserId = userId;
+            ViewBag.MovieId = movieId;
+            ViewBag.Score = reply.Score;
+            ViewBag.Recommended = reply.Recommended;
+            return View();
+        }
+    }
+}
